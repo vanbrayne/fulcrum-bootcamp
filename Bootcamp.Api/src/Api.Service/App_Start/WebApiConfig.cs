5a672f7cf5d42fc4b976621eee88e20040f51494
Emil Nilsson <emilnilssom89@gmail.com>
2017-12-05 16:28:32 +0100

    Added Api template

---
+++ Bootcamp.Api/src/Api.Service/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using Xlent.Lever.Authentication.Sdk.Handlers;
+using Xlent.Lever.Libraries2.WebApi.Pipe.Inbound;
+
+namespace Api.Service
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            //Declare the project to return JSON instead of XML
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            //Replace ExceptionHandler with a custom handler
+            config.Services.Replace(typeof(IExceptionHandler), new ConvertExceptionToFulcrumResponse());
+            config.MessageHandlers.Add(new SaveCorrelationId());
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+    }
+}
---
+++ Bootcamp.Api/src/Api.Service/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Api.Service.Models;
+using Xlent.Lever.Authentication.Sdk.Attributes;
+using Xlent.Lever.Libraries2.Core.Error.Logic;
+using Xlent.Lever.Libraries2.Core.Platform.Authentication;
+
+namespace Api.Service.Controllers
+{
+    [RoutePrefix("api/Users")]
+    [FulcrumAuthorize(AuthenticationRoleEnum.ExternalSystemUser)]
+    public class UserController : ApiController
+    {
+        /*
+         * Implementation of GET is a mandatory exercise
+         */
+        [Route("{id}")]
+        [HttpGet]
+        public User Get(string id)
+        {
+            throw new FulcrumNotImplementedException();
+        }
+
+        /*
+         * Implementation of POST is a mandatory exercise
+         */
+        [Route("")]
+        [HttpPost]
+        public string Post(User user)
+        {
+            throw new FulcrumNotImplementedException();
+        }
+
+        /*
+         * Implementation of PUT is a voluntary exercise
+         */
+        [Route("{id}")]
+        [HttpPut]
+        public User Put(string id, User user)
+        {
+            throw new FulcrumNotImplementedException();
+        }
+
+        /*
+         * Implementation of DELETE is a voluntary exercise
+         */
+        [Route("{id}")]
+        [HttpDelete]
+        public User Delete()
+        {
+            throw new FulcrumNotImplementedException();
+        }
+
+
+
+    }
+}
