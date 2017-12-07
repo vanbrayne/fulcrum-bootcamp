d1e94ca6414306ed3ae8f62cdc1e4ec894224142
Lars Lindberg <lars.lindberg@xlent.se>
2017-12-07 17:39:01 +0100

    Added logging to CustomerMaster and UserStatistics.

--- BootCamp.CustomerMaster/src/CustomerMaster.Service.FulcrumAdapter/App_Start/AutofacConfig.cs
+++ BootCamp.CustomerMaster/src/CustomerMaster.Service.FulcrumAdapter/App_Start/AutofacConfig.cs
@@ -1,11 +1,19 @@
-﻿using System.Reflection;
+﻿using System.Configuration;
+using System.Reflection;
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
 using CustomerMaster.Service.FulcrumAdapter.Contract;
+using Xlent.Lever.Authentication.Sdk;
+using Xlent.Lever.Libraries2.Core.Application;
 using Xlent.Lever.Libraries2.Core.MultiTenant.Context;
+using Xlent.Lever.Libraries2.Core.MultiTenant.Model;
+using Xlent.Lever.Libraries2.Core.Platform.Authentication;
 using Xlent.Lever.Libraries2.Core.Storage.Logic;
 using Xlent.Lever.Libraries2.Core.Storage.Model;
+using Xlent.Lever.Libraries2.WebApi.Platform.Authentication;
+using Xlent.Lever.Logger.Sdk;
+using Xlent.Lever.Logger.Sdk.RestClients;
 
 #pragma warning disable 1591
 
@@ -23,6 +31,43 @@ namespace CustomerMaster.Service.FulcrumAdapter
             builder.RegisterType<TenantConfigurationValueProvider>().As<ITenantConfigurationValueProvider>().SingleInstance();
             var container = builder.Build();
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            var organization = ConfigurationManager.AppSettings["Organization"];
+            var environment = ConfigurationManager.AppSettings["Environment"];
+            var tenant = new Tenant(organization, environment);
+            builder.RegisterInstance(tenant).As<ITenant>();
+
+            var tokenRefresher = RegisterAuthentication(builder, tenant);
+
+            RegisterLogging(tokenRefresher);
+        }
+
+        private static void RegisterLogging(ITokenRefresherWithServiceClient tokenRefresher)
+        {
+            // Logging
+            var loggerBaseUrl = FulcrumApplication.AppSettings.GetString("Logger.Url", true);
+            var logClient = new LogClient(loggerBaseUrl, tokenRefresher.GetServiceClient());
+            FulcrumApplication.Setup.FullLogger = new FulcrumLogger(logClient);
+        }
+
+        private static ITokenRefresherWithServiceClient RegisterAuthentication(ContainerBuilder builder, Tenant tenant)
+        {
+            var authenticationUrl = ConfigurationManager.AppSettings["Authentication.Url"];
+            var authenticationClientId = ConfigurationManager.AppSettings["Authentication.ClientId"];
+            var authenticationClientSecret = ConfigurationManager.AppSettings["Authentication.ClientSecret"];
+            var authServiceCredentials = new AuthenticationCredentials { ClientId = "user", ClientSecret = "pwd" };
+
+            IAuthenticationCredentials authTokenCredentials =
+                new AuthenticationCredentials
+                {
+                    ClientId = authenticationClientId,
+                    ClientSecret = authenticationClientSecret
+                };
+
+            var tokenRefresher = AuthenticationManager.CreateTokenRefresher(tenant, authenticationUrl,
+                authServiceCredentials, authTokenCredentials);
+            builder.RegisterInstance(tokenRefresher).As<ITokenRefresherWithServiceClient>();
+            return tokenRefresher;
         }
     }
 }
