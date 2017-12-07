9bc9a3582bd11d1ffb704fe76440fcc80c2c81d4
Lars Lindberg <lars.lindberg@xlent.se>
2017-12-07 14:33:01 +0100

    Renaming capability

---
+++ BootCamp.CustomerMaster/src/CustomerMaster.Service.FulcrumAdapter/App_Start/AutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using CustomerMaster.Service.FulcrumAdapter.Contract;
+using Xlent.Lever.Libraries2.Core.MultiTenant.Context;
+using Xlent.Lever.Libraries2.Core.Storage.Logic;
+using Xlent.Lever.Libraries2.Core.Storage.Model;
+
+#pragma warning disable 1591
+
+namespace CustomerMaster.Service.FulcrumAdapter
+{
+    public static class AutofacConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<MemoryPersistance<User, string>>().As<ICrud<User, string>>().SingleInstance();
+
+            builder.RegisterType<TenantConfigurationValueProvider>().As<ITenantConfigurationValueProvider>().SingleInstance();
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
---
+++ BootCamp.CustomerMaster/src/CustomerMaster.Service.FulcrumAdapter/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using CustomerMaster.Service.FulcrumAdapter;
+using WebActivatorEx;
+using Xlent.Lever.Authentication.Sdk.Handlers;
+using Xlent.Lever.Libraries2.WebApi.Pipe.Inbound;
+#pragma warning disable 1591
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace CustomerMaster.Service.FulcrumAdapter
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Services.Replace(typeof(IExceptionHandler), new ConvertExceptionToFulcrumResponse());
+            config.MessageHandlers.Add(new TokenValidationHandler());
+            config.MessageHandlers.Add(new SaveCorrelationId());
+            config.MessageHandlers.Add(new LogRequestAndResponse());
+        }
+    }
+}
