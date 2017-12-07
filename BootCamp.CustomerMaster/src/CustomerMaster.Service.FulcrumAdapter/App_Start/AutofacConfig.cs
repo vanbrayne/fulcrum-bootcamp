70817da5386933089540cd491616c2fd5f0ce0e7
Emil Nilsson <emilnilssom89@gmail.com>
2017-12-07 17:40:33 +0100

    Added authentication to CustomerMaster

--- BootCamp.CustomerMaster/src/CustomerMaster.Service.FulcrumAdapter/App_Start/AutofacConfig.cs
+++ BootCamp.CustomerMaster/src/CustomerMaster.Service.FulcrumAdapter/App_Start/AutofacConfig.cs
@@ -1,11 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Configuration;
+using System.Reflection;
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
 using CustomerMaster.Service.FulcrumAdapter.Contract;
+using Xlent.Lever.Authentication.Sdk;
 using Xlent.Lever.Libraries2.Core.MultiTenant.Context;
+using Xlent.Lever.Libraries2.Core.MultiTenant.Model;
+using Xlent.Lever.Libraries2.Core.Platform.Authentication;
 using Xlent.Lever.Libraries2.Core.Storage.Logic;
 using Xlent.Lever.Libraries2.Core.Storage.Model;
+using Xlent.Lever.Libraries2.WebApi.Platform.Authentication;
 
 #pragma warning disable 1591
 
@@ -18,11 +23,39 @@ namespace CustomerMaster.Service.FulcrumAdapter
             var builder = new ContainerBuilder();
 
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            RegisterDependencies(builder);
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static void RegisterDependencies(ContainerBuilder builder)
+        {
+            var organization = ConfigurationManager.AppSettings["Organization"];
+            var environment = ConfigurationManager.AppSettings["Environment"];
+            var tenant = new Tenant(organization, environment);
+            builder.RegisterInstance(tenant).As<ITenant>();
+
+            var authenticationUrl = ConfigurationManager.AppSettings["Authentication.Url"];
+            var authenticationClientId = ConfigurationManager.AppSettings["Authentication.ClientId"];
+            var authenticationClientSecret = ConfigurationManager.AppSettings["Authentication.ClientSecret"];
+            var authServiceCredentials = new AuthenticationCredentials { ClientId = "user", ClientSecret = "pwd" };
+
+            IAuthenticationCredentials authTokenCredentials =
+                new AuthenticationCredentials
+                {
+                    ClientId = authenticationClientId,
+                    ClientSecret = authenticationClientSecret
+                };
+
+            var tokenRefresher = AuthenticationManager.CreateTokenRefresher(tenant, authenticationUrl,
+                authServiceCredentials, authTokenCredentials);
+            builder.RegisterInstance(tokenRefresher).As<ITokenRefresherWithServiceClient>();
+
             builder.RegisterType<MemoryPersistance<User, string>>().As<ICrud<User, string>>().SingleInstance();
 
             builder.RegisterType<TenantConfigurationValueProvider>().As<ITenantConfigurationValueProvider>().SingleInstance();
-            var container = builder.Build();
-            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
